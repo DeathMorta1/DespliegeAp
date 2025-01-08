5770ea03a98738b9beb65198af7fde8175937464
DeathMorta1 <capitanadri12@gmail.com>
2025-01-08 11:16:36 +0100

    Login funcionando

--- LibrosRestToken/routes/auth.js
+++ LibrosRestToken/routes/auth.js
@@ -1,12 +1,20 @@
+// Importamos el módulo Express para manejar rutas y peticiones
 const express = require('express');
-const auth = require(__dirname + '/../utils/auth.js');
 
+// Importamos el módulo de autenticación personalizado desde utils/auth
+const auth = require(__dirname + '/../utils/auth');
+
+// Creamos un enrutador de Express para definir las rutas de autenticación
 let router = express.Router();
 
+// Simulamos la base de datos así
 const usuarios = [
-    { usuario: 'yo', password: '1234' }
+    { usuario: 'nacho', password: '12345' },
+    { usuario: 'alex', password: 'alex111' },
+    { usuario: 'yo', password: '1234'}
 ];
 
+// Ruta para manejar el inicio de sesión
 router.post('/login', (req, res) => {
     let usuario = req.body.usuario;
     let password = req.body.password;
@@ -19,4 +27,5 @@ router.post('/login', (req, res) => {
         res.send({ok: false});
 });
 
+// Exportamos el enrutador para usarlo en otros módulos de la aplicación
 module.exports = router;
