3b2cab07ac47b849f56435a9736eabf00633454d
DeathMorta1 <capitanadri12@gmail.com>
2025-01-12 21:53:13 +0100

    Arreglamos el login de usuario

--- PhysioCare/routes/auth.js
+++ PhysioCare/routes/auth.js
@@ -10,17 +10,18 @@ let User = require(__dirname+'/../models/users.js');
 
 
 // Ruta para manejar el inicio de sesión
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
     let login = req.body.login;
     let password = req.body.password;
-    let existeUsuario = User.filter(u =>
-        u.login == login && u.password == password);
 
-    if (existeUsuario.length == 1)
+    let existeUsuario = await User.find({login:login,password:password});
+
+    if (existeUsuario.length > 0)
         res.send({ok: true, token: auth.generarToken(existeUsuario[0].login,
-                                                     existeUsuario[0].rol)});
+                                                        existeUsuario[0].rol)});
     else
         res.send({ok: false, message:"login incorrecto"});
+    
 });
 
 // Exportamos el enrutador para usarlo en otros módulos de la aplicación
